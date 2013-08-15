605bf3d3104c5e60b91a098a09170969798e4f7e
Doug McGeehan <djmvfb@mst.edu>
2013-08-15 07:43:52 -0500

    Settings and profiles updated

---
+++ profiles/default.py
@@ -0,0 +1,19 @@
+# DIMENSIONS AREN'T CHANGING ANY TIME SOON! 
+SOUNDS = [
+    ['Reggae Airhorn.wav', 'Meow.wav',],     # colors[0][0], colors[0][1]
+    ['Ha! GAAAAAY.wav', 'Gun Battle.wav',],
+    ['black', 'black',],
+    ['black', 'black',],
+    ['black', 'black',],
+    ['cyan', 'cyan',],
+    ['cyan', 'cyan',],
+    ['cyan', 'cyan',],
+    ['cyan', 'cyan',],
+    ['cyan', 'cyan',],
+    ['yellow', 'yellow',],
+    ['yellow', 'yellow',],
+    ['yellow', 'yellow',],
+    ['yellow', 'yellow',],
+    ['yellow', 'yellow',],   # colors[14][0], colors[14][1]
+]
+
